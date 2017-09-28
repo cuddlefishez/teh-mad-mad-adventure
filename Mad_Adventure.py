11e8b110744c24fe9064087f81fc32fe1ecf99fb
cuddlefishez <habermillersean@gmail.com>
2017-09-27 18:58:08 -0600

    Just added critical strikes and random start gold
    Basic stuff

--- Mad_Adventure.py
+++ Mad_Adventure.py
@@ -17,13 +17,7 @@ someone will have to create tables for encounters so we have custom ones for eve
 """
 table_dict = {'plains':3}
 keyed_dict = {3: [{ 1:'Goblin'},{2:'Hobgoblin'},{3:'Ancient Black Dragon'}]}
-"""
-The following list is a classification of weapontypes it is a multidimensional array
-first the weapon classification as simple, simple ranged, martial or martial ranged acts as a pointer
-then the weapon name points to ward the particular properties of that weapon as follows, cost, damage dice, damage type 
-(b for blugeoning, s for slashing, p for piercing), weight in lbs., any other specific properties 
-note: in specific properties thrown1 is an indication of range (20,60) ft thrown2 is indication of (30,120) ft
-"""
+
 # this class creates combat instances, not sure if I still want it to be this way, however it may be the best way to add story combats 
 class Combat(object):
 	def __init__(self):
@@ -36,8 +30,8 @@ class Combat(object):
 			for x in self.intiative:
 				if x in self.Monsters:
 					k = x.Attack()
-					if k >= self.player.AC:
-						y = x.Dmg()
+					if k[0] >= self.player.AC:
+						y = x.Dmg(k[1])
 						print ('The '+str(x.name)+' strikes you for '+str(y)+' damage!')
 						self.player.TakeDmg(y)
 						if self.player.state == 'dead':
@@ -61,8 +55,8 @@ class Combat(object):
 						else: 
 							continue
 					k = x.Attack()
-					if k >= monst.AC:
-						y = x.Dmg()
+					if k[0] >= monst.AC:
+						y = x.Dmg(k[1])
 						print ('Your strike is true! You do '+str(y)+' damage to the '+str(monst.name)+'!')
 						monst.TakeDmg(y)
 						if monst.state == 'dead':
@@ -95,16 +89,26 @@ class Monster(object):
 		self.int = None
 		self.wis = None
 		self.cha = None
+		self.stats = None
+	def Stats(self):
+		self.stats = {'Strength': self.str,'Dexterity':self.dex,'Constitution':self.con,'Intellegence':self.int,'Wisdom':self.wis,'Charisma':self.cha}
 	def Attack(self):
-		x = twenty.Roll() + ((self.str-10)/2)//1
-		return(x)
-	def Dmg(self):
+		c = 0
+		roll = twenty.Roll()
+		if roll == 20:
+			c = 1
+			print("A dangerous strike!")
+		mod = ((self.str-10)/2)//1
+		x = roll + mod 
+		return(int(x),c)
+	def Dmg(self,c):
 		x = weapon_info(self.weapon,Weapons)
 		if self.weapon == 'Fist':
 			dmg = 1 + ((self.str-10)/2)//1
 		else:
 			dmg = x[1][1][1].Roll() + ((self.str-10)/2)//1
-		return(dmg)
+		
+		return(dmg*(c+1))
 	def TakeDmg(self,dmg):
 		self.health = self.health - dmg
 		if self.health <= 0:
@@ -127,7 +131,7 @@ class Player(object):
 		self.inventory = []
 		self.weapon = None
 		self.spells = []
-		self.gold = None
+		self.money = {'gp': 0, 'sp': 0, 'cp': 0}
 		self.AC = None
 		self.initiative = None
 		self.state = None
@@ -142,24 +146,38 @@ class Player(object):
 		self.Access_Inv()
 		print(self.stats)
 	def Attack(self):
-		x = twenty.Roll() + (self.stats['Strength']-10)/2//1
-		return (x)
-	def Dmg(self):
+	# handles player attacks
+		c = 0
+		roll = twenty.Roll()
+		if roll == 20:
+			c = 1
+			print("That's a critical strike!!")
+		mod = ((self.stats['Strength']-10)/2)//1
+		x = roll + mod 
+		return(int(x),c)
+	def Dmg(self,c):
+	#handles player dmg
 		x = weapon_info(self.weapon,Weapons)
 		if self.weapon == 'Fist':
 			dmg = 1 + ((self.stats['Strength']-10)/2)//1
 		else:
 			dmg = x[1][1][1].Roll() + ((self.stats['Strength']-10)/2)//1
-		return(dmg)
+		
+		return(dmg*(c+1))
 	def TakeDmg(self,dmg):
+	#handles dmg recieved
 		self.health = self.health - dmg
 		if int(self.health) <= 0:
 			print('You are dead')
 			self.state = 'dead'
 		else:
 			print('You have '+str(self.health)+' remaining')
-			
-		
+	def starting_gold(self):
+	#gives starting gold
+		gold = {'Barbarian': [2,four,10] ,'Bard': [5,four,10],'Cleric':[5,four,10],'Druid':[2,four,10],'Fighter':[5,four,10],'Monk':[5,four,1],'Paladin':[5,four,10],'Ranger':[5,four,10],'Rogue':[4,four,10],'Sorcerer':[3,four,10],'Warlock':[4,four,10],'Wizard':[4,four,10]}
+		g = gold[self.player_class]
+		gp = int(g[1].multiRoll(g[0])*g[2])
+		self.money['gp'] = gp
 	def Alive(self):
 		if self.state == 'dead':
 			print('You Are Dead')
@@ -178,9 +196,10 @@ class d12(object):
 		self.roll = random.randint(1,12)
 		return(self.roll)
 	def multiRoll(self,n):
-		for i in range(0,n):
+		for i in range(0,n+1):
 			x = self.Roll()
 			self.total += x
+		return(int(self.total))
 class d10(object):
 	def __init__(self):
 		self.roll = 0
@@ -189,9 +208,10 @@ class d10(object):
 		self.roll = random.randint(1,10)
 		return(self.roll)
 	def multiRoll(self,n):
-		for i in range(0,n):
+		for i in range(0,n+1):
 			x = self.Roll()
 			self.total += x
+		return(int(self.total))
 class d8(object):
 	def __init__(self):
 		self.roll = 0
@@ -200,9 +220,10 @@ class d8(object):
 		self.roll = random.randint(1,8)
 		return(self.roll)
 	def multiRoll(self,n):
-		for i in range(0,n):
+		for i in range(0,n+1):
 			x = self.Roll()
 			self.total += x
+		return(int(self.total))
 class d6(object):
 	def __init__(self):
 		self.roll = 0
@@ -211,9 +232,10 @@ class d6(object):
 		self.roll = random.randint(1,6)
 		return(self.roll)
 	def multiRoll(self,n):
-		for i in range(0,n):
+		for i in range(0,n+1):
 			x = self.Roll()
 			self.total += x
+		return(int(self.total))
 class d4(object):
 	def __init__(self):
 		self.roll = 0
@@ -222,9 +244,10 @@ class d4(object):
 		self.roll = random.randint(1,4)
 		return(self.roll)
 	def multiRoll(self,n):
-		for i in range(0,n):
+		for i in range(0,n+1):
 			x = self.Roll()
 			self.total += x
+		return(int(self.total))
 class d100(object):
 	def __init__(self):
 		self.roll = 0
@@ -238,7 +261,7 @@ def player_iniative(Player):
 	return(r)
 
 def monster_iniative(Monster):
-	r = monster_check(Monster)
+	r = monster_check(Monster,'Dexterity')
 	return(r)
 
 	
@@ -261,7 +284,8 @@ def intiative_order(monsters,player):
 
 def weapon_assignment(Weapons,choice):
 	# Weapons is list
-	# Choice is string list to expand choice of weapon based on type shown below in access_lv
+	# Choice is string list to expand choice of weapon based on type shown below in access_lv this function is mearly
+	# to introduce random weapons to enemys and the player, a shop will be set up for starting equipment inchar creation
 	access_lv = {'simple':0, 'simple_r':1,'martial':2,'martial_r':3}
 	a = []
 	weap = []
@@ -271,6 +295,16 @@ def weapon_assignment(Weapons,choice):
 		weap.append(Weapons[n][random.randint(1,len(Weapons[n])-1)][0])
 	weapon = weap[random.randint(0,len(weap)-1)]
 	return(weapon)
+def weapon_shop_inital(Weapons,choice):
+	access_lv = {'simple':0, 'simple_r':1,'martial':2,'martial_r':3}
+	a = []
+	weap = []
+	for choice in choice:
+		a.append(access_lv[choice])
+	for n in a:
+		for l in range(1,len(Weapons[n])):
+			weap.append(Weapons[n][1][0])
+	return(weap)
 def weapon_info(weapon,Weapons):
 	#weapon is a string
 	#Weapons is a multi dimensional list
@@ -282,8 +316,8 @@ def weapon_info(weapon,Weapons):
 				continue
 	return(k)
 def Stat_Roller(): 
-#stat roller function for character creation 
-	stat = random.randint(1,6) + random.randint(1,6) + random.randint(1,6)
+#stat roller function for character creation currently curved to higher stats
+	stat = random.randint(3,6) + random.randint(3,6) + random.randint(3,6)
 	return(stat)
 def Stat_Assignment():
 # stat assignment function for character creation 
@@ -394,6 +428,13 @@ ten = d10()
 eight = d8()
 six = d6()
 four = d4()
+"""
+The following list is a classification of weapontypes it is a multidimensional array
+first the weapon classification as simple, simple ranged, martial or martial ranged acts as a pointer
+then the weapon name points to ward the particular properties of that weapon as follows, cost, damage dice, damage type 
+(b for blugeoning, s for slashing, p for piercing), weight in lbs., any other specific properties 
+note: in specific properties thrown1 is an indication of range (20,60) ft thrown2 is indication of (30,120) ft
+"""
 Weapons = [['simple',['Club',[[1,'sp'],[1,four],'b',2,['light']]],['Dagger',[[2,'gp'],[1,four],'p',1,['finesse','light','thrown1']]],['Greatclub',[[2,'sp'],[1,eight],'b',10,['two-handed']]],['Handaxe',[[5,'gp'],[1,six],'s',2,['light','thrown1']]],['Javalin',[[5,'sp'],[1,six],'p',2,['thrown2']]],['Light-Hammer',[[2,'gp'],[1,four],'b',2,['light','thrown1']]],['Mace',[[5,'gp'],[1,six],'b',4,['Light']]],['Quaterstaff',[[2,'sp'],[1,six],'b',4,['versatile']]],['Sickle',[[1,'gp'],[1,four],'s',2,['light']]],['Spear',[[1,'gp'],[1,six],'p',3,['thrown1','versatile']]],['Fist',[[0,''],1,'b','','']]],['simple_r'],['martial'],['martial_r']]
 def skill_check(stat, stats, proficiency,bonus):
 #player skill checks 
@@ -401,10 +442,11 @@ def skill_check(stat, stats, proficiency,bonus):
 	x = twenty.Roll()
 	check = x + mod + proficiency + bonus
 	return(int(check))
-def monster_check(monster):
-#stat must be string (dex,st,con,ect)
+def monster_check(monster,stat):
+#stat must be string (Dexterity,so on)
 #monster skill checks currently broken
-	mod = ((monster.dex-10)/2)//1
+	
+	mod = ((monster.stats[stat]-10)/2)//1
 	x = twenty.Roll()
 	check = x + mod 
 	return(int(check))
@@ -413,6 +455,7 @@ char = Player()
 char.name = input('I require only your name! The rest is up to you! ')
 char.level = 1
 char.player_class = player_class()
+char.starting_gold()
 char.hitdice = hitdice(char.player_class)
 char.stats = Stat_Assignment()
 char.weapon = weapon_assignment(Weapons,['simple'])
@@ -423,7 +466,7 @@ char.AC = 10 + ((char.stats['Dexterity']-10)/2)//1
 char.state = 'alive'
 
 print('Well done '+char.name+' now there are only a few more questions you must answer!')
-
+print(char.money['gp'])
 clearing = Room()
 clearing.initial_text = 'Its been seven days since you set out from your small town to make for the city of Whitewater. Last night you decided to camp off the road. It is early morning now, roll perception!'
 clearing.mchecks = {'Perception' : 15}
@@ -433,7 +476,12 @@ clearing.monsters = Monster()
 clearing.monsters.name = clearing.monster
 clearing.monsters.str = monster_info[0]
 clearing.monsters.dex = monster_info[1]
+clearing.monsters.con = monster_info[2]
+clearing.monsters.int = monster_info[3]
+clearing.monsters.wis = monster_info[4]
+clearing.monsters.cha = monster_info[5]
 clearing.monsters.AC = monster_info[8]
+clearing.monsters.Stats()
 clearing.monsters.health = monster_info[6]
 clearing.monsters.state = 'alive'
 if monster_info[10] == 1:
@@ -481,6 +529,8 @@ elif link == 2:
 		clearing1.initial_text = 'You hear a shout and open your eyes to a '+clearing.monsters.weapon.lower()+' coming down toward you'
 		link = 'a2'
 if link == 'a2':
+	
+	
 	monsters = [clearing.monsters]
 	print(clearing1.initial_text)
 	a = intiative_order(monsters,char)
