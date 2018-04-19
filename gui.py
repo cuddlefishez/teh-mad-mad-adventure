b611d10b71116bd3097a7cb88288bfcb8551b191
cuddlefishez <habermillersean@gmail.com>
2018-04-19 20:34:48 -0300

    Code works but having a hard time trying to pull inputs from GUI

--- gui.py
+++ gui.py
@@ -8,10 +8,22 @@ class Application(Frame):
 		self.title.pack()
 		self.w.pack()
 		self.q.pack()
+	def input_get(self):
+		x = self.e.get()
+		print(x)
+	def character_name(self):
+		string_1 = "Welcome to a world of magic and Adventure!! I require only your name!!" 
+		self.s = Text(self)
+		self.s.insert(INSERT, string_1)
+		self.e = Entry(self)
+		self.e.focus()
 	def Startup(self):
 		self.w.pack_forget()
 		self.q.pack_forget()
 		self.title.pack_forget()
+		self.character_name()
+		self.s.pack()
+		self.e.pack()
 	def __init__(self, master=None):
 		Frame.__init__(self, master,width = 1980, height = 1080, bg = 'black')
 		self.pack_propagate(0) 
